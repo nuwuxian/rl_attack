c9a1590fdda1853f3adf367dc90b298d976eb4e4
psuwuxian <sysuwuxian@gmail.com>
2020-09-15 21:45:38 -0400

    Update run_retrain.sh

--- MuJoCo/src/run_retrain.sh
+++ MuJoCo/src/run_retrain.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for i in 0 1 2 3 4; do
-    python victim_train.py 2 --x_method='grad' > console_$i.txt &
+    python victim_train.py --x_method='grad' > console_$i.txt &
     sleep 10
 done
